40e9c874c2777fe6bc38c7aa96646a06bdc7b02d
ksh103 <jayy_z@naver.com>
2022-10-24 10:25:26 +0900

    chore : SwaggerConfig 설정

---
+++ backend/src/main/java/kr/sosu/dailyline/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package kr.sosu.dailyline.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kr.sosu.dailyline"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Daily Line API")
+                .description("Daily Line")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("0.1")
+                .build();
+    }
+}
