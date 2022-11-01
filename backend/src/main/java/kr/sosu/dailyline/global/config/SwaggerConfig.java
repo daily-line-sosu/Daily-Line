9139d5a704dc7d597cc9633b866def8888de80f1
ksh103 <jayy_z@naver.com>
2022-11-01 22:25:14 +0900

    chore : SwaggerConfig 재설정

--- backend/src/main/java/kr/sosu/dailyline/global/config/SwaggerConfig.java
+++ backend/src/main/java/kr/sosu/dailyline/global/config/SwaggerConfig.java
@@ -1,35 +1,26 @@
 package kr.sosu.dailyline.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("kr.sosu.dailyline"))
-                .paths(PathSelectors.ant("/api/**"))
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Daily Line API")
-                .description("Daily Line")
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("0.1")
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("daily-line")
+                .pathsToMatch("/api/**")
                 .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Daily Line API")
+                        .description("Daily Line API 명세서입니다.")
+                        .version("v0.0.1"));
     }
 }
