38bfc9aca6c19cff22670a3f819ab8cddd3135e7
ksh103 <jayy_z@naver.com>
2022-11-04 20:43:36 +0900

    chore : RedisSessionConfig 수정

--- backend/src/main/java/kr/sosu/dailyline/global/config/RedisSessionConfig.java
+++ backend/src/main/java/kr/sosu/dailyline/global/config/RedisSessionConfig.java
@@ -3,8 +3,10 @@ package kr.sosu.dailyline.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.data.redis.RedisSessionRepository;
@@ -14,6 +16,12 @@ import java.time.Duration;
 @EnableSpringHttpSession
 @Configuration
 public class RedisSessionConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
 
     @Bean
     public RedisOperations<String, Object> sessionRedisOperations(RedisConnectionFactory redisConnectionFactory) {
@@ -27,12 +35,19 @@ public class RedisSessionConfig {
     }
 
     @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
+    }
+    @Bean
     public RedisSessionRepository sessionRepository(RedisOperations<String, Object> sessionRedisOperations) {
         RedisSessionRepository redisSessionRepository = new RedisSessionRepository(sessionRedisOperations);
         redisSessionRepository.setDefaultMaxInactiveInterval(Duration.ofSeconds(300));
 
         return redisSessionRepository;
     }
-
 }
 
